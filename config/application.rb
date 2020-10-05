9e7c82463115edef0d9a5f61356669c1b6e77e44
yash upadhyay <upadhyayyash923@gmail.com>
2020-10-05 13:52:26 +0530

    rack-cors

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module Details
   class Application < Rails::Application
     config.load_defaults 6.0
 
-    config.api_only = true
-
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
